423017dca3f6bcc3b0aefc6f76ccca74dfb013d2
peaches6 <skpikao233@gmail.com>
2018-02-10 17:27:55 +0000

    Calculator runs forever, and processes decimals

--- calculator.py
+++ calculator.py
@@ -1,23 +1,51 @@
-num1 = int(input("Enter a number (Not a decimal):"))
-print("")
-num2 = int(input("Enter another number (Not a decimal):"))
-print("")
-operation = input("Enter '+' for addition, '-' for subtraction, '*' for multiplication, and '/' for division,")
 
-if operation == '+':
-	answer = (num1 + num2)
-	print("")
-	print(answer)
-elif operation == '-':
-	answer = (num1 - num2)
-	print("")
-	print(answer)
-elif operation == '*':
-	answer = (num1 * num2)
-	print("")
-	print(answer)
-elif operation == '/':
-	answer = (num1 / num2)
-	print("")
-	print(answer)
 
+
+def StartCalculator():
+
+    num1 = float(input("Enter a number :"))
+
+    print("")
+
+    num2 = float(input("Enter another number :"))
+
+    print("")
+
+    operation = input("Enter '+' for addition, '-' for subtraction, '*' for multiplication, and '/' for division,")
+
+
+
+    if operation == '+':
+
+            answer = (num1 + num2)
+
+            print("")
+
+            print(answer)
+
+    elif operation == '-':
+
+            answer = (num1 - num2)
+
+            print("")
+
+            print(answer)
+
+    elif operation == '*':
+
+            answer = (num1 * num2)
+
+            print("")
+
+            print(answer)
+
+    elif operation == '/':
+
+            answer = (num1 / num2)
+
+            print("")
+
+            print(answer)
+
+while(True):
+    StartCalculator()
